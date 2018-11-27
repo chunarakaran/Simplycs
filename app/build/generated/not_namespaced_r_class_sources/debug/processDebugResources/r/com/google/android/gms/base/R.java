ffd832816aa3fbea185f3639a0b65357754f0ae1
karan <chunarakaran@gmail.com>
2018-11-27 16:33:30 +0530

    changes

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -15,9 +15,9 @@ public final class R {
         public static final int buttonSize = 0x7f040056;
         public static final int circleCrop = 0x7f04007b;
         public static final int colorScheme = 0x7f040093;
-        public static final int imageAspectRatio = 0x7f040100;
-        public static final int imageAspectRatioAdjust = 0x7f040101;
-        public static final int scopeUris = 0x7f04018d;
+        public static final int imageAspectRatio = 0x7f040114;
+        public static final int imageAspectRatioAdjust = 0x7f040115;
+        public static final int scopeUris = 0x7f0401c8;
     }
     public static final class color {
         private color() {}
@@ -37,40 +37,40 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int common_full_open_on_phone = 0x7f080067;
-        public static final int common_google_signin_btn_icon_dark = 0x7f080068;
-        public static final int common_google_signin_btn_icon_dark_focused = 0x7f080069;
-        public static final int common_google_signin_btn_icon_dark_normal = 0x7f08006a;
-        public static final int common_google_signin_btn_icon_dark_normal_background = 0x7f08006b;
-        public static final int common_google_signin_btn_icon_disabled = 0x7f08006c;
-        public static final int common_google_signin_btn_icon_light = 0x7f08006d;
-        public static final int common_google_signin_btn_icon_light_focused = 0x7f08006e;
-        public static final int common_google_signin_btn_icon_light_normal = 0x7f08006f;
-        public static final int common_google_signin_btn_icon_light_normal_background = 0x7f080070;
-        public static final int common_google_signin_btn_text_dark = 0x7f080071;
-        public static final int common_google_signin_btn_text_dark_focused = 0x7f080072;
-        public static final int common_google_signin_btn_text_dark_normal = 0x7f080073;
-        public static final int common_google_signin_btn_text_dark_normal_background = 0x7f080074;
-        public static final int common_google_signin_btn_text_disabled = 0x7f080075;
-        public static final int common_google_signin_btn_text_light = 0x7f080076;
-        public static final int common_google_signin_btn_text_light_focused = 0x7f080077;
-        public static final int common_google_signin_btn_text_light_normal = 0x7f080078;
-        public static final int common_google_signin_btn_text_light_normal_background = 0x7f080079;
-        public static final int googleg_disabled_color_18 = 0x7f080081;
-        public static final int googleg_standard_color_18 = 0x7f080082;
+        public static final int common_full_open_on_phone = 0x7f080069;
+        public static final int common_google_signin_btn_icon_dark = 0x7f08006a;
+        public static final int common_google_signin_btn_icon_dark_focused = 0x7f08006b;
+        public static final int common_google_signin_btn_icon_dark_normal = 0x7f08006c;
+        public static final int common_google_signin_btn_icon_dark_normal_background = 0x7f08006d;
+        public static final int common_google_signin_btn_icon_disabled = 0x7f08006e;
+        public static final int common_google_signin_btn_icon_light = 0x7f08006f;
+        public static final int common_google_signin_btn_icon_light_focused = 0x7f080070;
+        public static final int common_google_signin_btn_icon_light_normal = 0x7f080071;
+        public static final int common_google_signin_btn_icon_light_normal_background = 0x7f080072;
+        public static final int common_google_signin_btn_text_dark = 0x7f080073;
+        public static final int common_google_signin_btn_text_dark_focused = 0x7f080074;
+        public static final int common_google_signin_btn_text_dark_normal = 0x7f080075;
+        public static final int common_google_signin_btn_text_dark_normal_background = 0x7f080076;
+        public static final int common_google_signin_btn_text_disabled = 0x7f080077;
+        public static final int common_google_signin_btn_text_light = 0x7f080078;
+        public static final int common_google_signin_btn_text_light_focused = 0x7f080079;
+        public static final int common_google_signin_btn_text_light_normal = 0x7f08007a;
+        public static final int common_google_signin_btn_text_light_normal_background = 0x7f08007b;
+        public static final int googleg_disabled_color_18 = 0x7f080087;
+        public static final int googleg_standard_color_18 = 0x7f080088;
     }
     public static final class id {
         private id() {}
 
-        public static final int adjust_height = 0x7f0a0020;
-        public static final int adjust_width = 0x7f0a0021;
-        public static final int auto = 0x7f0a0026;
-        public static final int dark = 0x7f0a0045;
-        public static final int icon_only = 0x7f0a0071;
-        public static final int light = 0x7f0a0083;
-        public static final int none = 0x7f0a00a2;
-        public static final int standard = 0x7f0a00e2;
-        public static final int wide = 0x7f0a0114;
+        public static final int adjust_height = 0x7f0a0025;
+        public static final int adjust_width = 0x7f0a0026;
+        public static final int auto = 0x7f0a002c;
+        public static final int dark = 0x7f0a004b;
+        public static final int icon_only = 0x7f0a0079;
+        public static final int light = 0x7f0a008b;
+        public static final int none = 0x7f0a00ab;
+        public static final int standard = 0x7f0a00eb;
+        public static final int wide = 0x7f0a011d;
     }
     public static final class string {
         private string() {}
@@ -96,11 +96,11 @@ public final class R {
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] LoadingImageView = { 0x7f04007b, 0x7f040100, 0x7f040101 };
+        public static final int[] LoadingImageView = { 0x7f04007b, 0x7f040114, 0x7f040115 };
         public static final int LoadingImageView_circleCrop = 0;
         public static final int LoadingImageView_imageAspectRatio = 1;
         public static final int LoadingImageView_imageAspectRatioAdjust = 2;
-        public static final int[] SignInButton = { 0x7f040056, 0x7f040093, 0x7f04018d };
+        public static final int[] SignInButton = { 0x7f040056, 0x7f040093, 0x7f0401c8 };
         public static final int SignInButton_buttonSize = 0;
         public static final int SignInButton_colorScheme = 1;
         public static final int SignInButton_scopeUris = 2;
--- app/src/main/java/com/example/amita/simplycs/MainActivity.java
+++ app/src/main/java/com/example/amita/simplycs/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.amita.simplycs.Adapter.SessionManager;
 import com.example.amita.simplycs.Fragment.AccountFragment;
@@ -99,30 +100,54 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//
-//            logoutUser();
-//
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_menu1) {
+
+            Toast.makeText(getApplicationContext(),"Ask Us",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        if (id == R.id.action_menu2) {
+
+            Toast.makeText(getApplicationContext(),"Last Activity",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        if (id == R.id.action_menu3) {
+
+            Toast.makeText(getApplicationContext(),"Discussion Forum",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        if (id == R.id.action_menu4) {
+
+            Toast.makeText(getApplicationContext(),"Webinars",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        if (id == R.id.action_menu5) {
+
+            Toast.makeText(getApplicationContext(),"Up-Comming Exam",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
